6c830dc4cd624b869e60d1aa337a1f8fe3aebbd9
BenGOD <bnrdbk@gmail.com>
2020-05-10 12:06:28 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function hello() {
+    alert(`Baby I Love You 😍😍😍😘💋💋💋🌹🌹🌹🌹🌹💋`)
+}
+
+function hi() {
+    alert(`Baby I Love You
+    Thanks for all the Love You have shown me
+    💖💖💖💖💖💖💖💖💖💖💖💖💖💖💖💖💖 `)
+}
