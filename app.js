508ca26f9556c976e1273b4ff938dae99c275e79
BenGOD <bnrdbk@gmail.com>
2020-05-10 13:55:28 +0000

    updates

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+alert("FROM BEN TO YOU ....On this exceptional day i wish you all the best ❤💕💖🌹Baby gal")
+
 function hello() {
     alert(`Baby I Love You 😍😍😍😘💋💋💋🌹🌹🌹🌹🌹💋`)
 }
 
 function hi() {
     alert(`Baby I Love You
-    Thanks for all the Love You have shown me
+    Thanks for all the Love You have showed me
     💖💖💖💖💖💖💖💖💖💖💖💖💖💖💖💖💖 `)
 }
